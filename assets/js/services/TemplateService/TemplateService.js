afabb99985dfed1eecce444e174c906802443fcb
Gavin Palmer <gavinpalmer1984@gmail.com>
2019-05-25 15:33:23 -0500

    formatSeconds with less than one second

--- assets/js/services/TemplateService/TemplateService.js
+++ assets/js/services/TemplateService/TemplateService.js
@@ -7,6 +7,9 @@ export const TemplateService = {
         return $.get(`${this.root}/${url}`);
     },
     formatSeconds(totalSeconds) {
+        if (totalSeconds < 1) {
+            return `${totalSeconds} seconds`;
+        }
         const hours = parseInt(totalSeconds / 3600, 10);
         const minutes = parseInt((totalSeconds % 3600) / 60, 10);
         const seconds = parseInt((totalSeconds % 60), 10);
