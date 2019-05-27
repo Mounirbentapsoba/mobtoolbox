afaff220102660322f1ecd63f92deb54a01e4b08
Gavin Palmer <gavinpalmer1984@gmail.com>
2019-05-27 18:31:33 -0500

    Update DriveNowView.js

--- assets/js/views/DriveNowView/DriveNowView.js
+++ assets/js/views/DriveNowView/DriveNowView.js
@@ -1,6 +1,6 @@
 import {
     MobService
-} from '/assets/js/services/MobService/MobService.js';
+} from '../../services/MobService/MobService.js';
 
 export const DriveNowView = Backbone.View.extend({
     el: '#main',
