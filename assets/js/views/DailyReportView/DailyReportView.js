7ef143c7ac0f2fd8e61660bbf3ffbba0807d5757
Gavin Palmer <gavinpalmer1984@gmail.com>
2019-10-10 19:47:00 -0500

    relative path for gh-pages

--- assets/js/views/DailyReportView/DailyReportView.js
+++ assets/js/views/DailyReportView/DailyReportView.js
@@ -1,6 +1,6 @@
 import {
     MobService
-} from '/assets/js/services/MobService/MobService.js';
+} from '../../services/MobService/MobService.js';
 
 export const DailyReportView = Backbone.View.extend({
     el: '#main',
